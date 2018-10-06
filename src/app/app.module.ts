fafd487b7943f1a3b1915676cfdbba3b32b81d03
Martin Filo <martinxfilo@gmail.com>
2018-10-06 17:28:39 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {Route, RouterModule, Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {ThreeComponent} from './components/three/three.component';
