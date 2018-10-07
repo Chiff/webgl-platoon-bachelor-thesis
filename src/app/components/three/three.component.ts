5dc9a380f92262a4c19a1c3ca850a335ca49e768
Martin Filo <martinxfilo@gmail.com>
2018-10-07 12:10:42 +0200

    three via mpm

--- src/app/components/three/three.component.ts
+++ src/app/components/three/three.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-
-declare var THREE;
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-three',
@@ -8,6 +7,11 @@ declare var THREE;
   styleUrls: ['./three.component.scss']
 })
 export class ThreeComponent implements OnInit {
+  dae: any;
+  loader: any;
+  scene: THREE.Scene = new THREE.Scene();
+  camera: THREE.Camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  renderer: THREE.Renderer;
 
   constructor() {
   }
@@ -17,12 +21,16 @@ export class ThreeComponent implements OnInit {
   }
 
   createScene() {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-    const renderer = new THREE.WebGLRenderer({
+    this.renderer = new THREE.WebGLRenderer(<THREE.WebGLRendererParameters>{
       canvas: document.getElementById('three')
     });
+
+    this.loader = new THREE.Loader();
+  }
+
+  loadObject(collada: THREE.ColladaModel) {
+    this.dae = collada.scene;
+
   }
 
 }
