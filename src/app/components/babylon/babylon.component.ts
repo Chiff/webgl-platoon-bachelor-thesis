4d0f66d69bc1b35cf8df757f38f6e3b63e7a61f0
Martin Filo <martinxfilo@gmail.com>
2018-10-13 16:34:18 +0200

    ovladanie v babylonjs

--- src/app/components/babylon/babylon.component.ts
+++ src/app/components/babylon/babylon.component.ts
@@ -12,6 +12,9 @@ export class BabylonComponent implements OnInit {
     scene: BABYLON.Scene;
     engine: BABYLON.Engine;
     camera: BABYLON.Camera;
+    materials: BABYLON.Material[];
+    meshes: BABYLON.AbstractMesh[];
+    speed = 0;
 
     constructor() {
     }
@@ -38,7 +41,7 @@ export class BabylonComponent implements OnInit {
         this.camera = new BABYLON.ArcRotateCamera(
             'camera1',
             0, 0, 10,
-            new BABYLON.Vector3(0, 0, 0),
+            new BABYLON.Vector3(1, 2, 2),
             this.scene
         );
 
@@ -50,29 +53,65 @@ export class BabylonComponent implements OnInit {
             new BABYLON.Vector3(0, 1, 0),
             this.scene
         );
+    }
 
+    loadObject() {
         BABYLON.SceneLoader.Append('assets/', 'kamion.babylon', this.scene, (newScene: BABYLON.Scene) => {
             const scene = newScene;
-            console.log(scene.materials);
-            scene.materials.map((material) => {
+            console.log(scene);
+            this.materials = scene.materials.map((material) => {
                 if (!material.id.includes('kamion')) {
-                    return;
+                    return null;
                 }
                 if (material.id.includes('naklad')) {
-                    material.diffuseTexture = new BABYLON.Texture('assets/naklad.png', scene);
+                    this.setTexture(material, 'assets/naklad.png', scene);
+                    this.changeColor(material, 0.8, 0.6, 0.3);
+                    this.setTexture(material, 'assets/naklad.png', scene);
                 } else {
-                    material.diffuseTexture = new BABYLON.Texture('assets/kamion.png', scene);
+                    this.setTexture(material, 'assets/kamion.png', scene);
                 }
-            });
+
+                return material;
+            }).filter((item: any) => item !== null);
+
+            this.meshes = scene.meshes;
+            this.engine.runRenderLoop(() => this.animate());
         }, (e) => {
             // console.log(e);
         }, (e) => {
             console.log(e);
         });
 
-        this.engine.runRenderLoop(() => this.scene.render());
     }
 
-    loadObject() {
+    find(array: any, key: string, search: string): any[] {
+        return array.filter((item: any) => {
+            return item[key].includes(search);
+        });
+    }
+
+    changeColor(object: BABYLON.Material, r: number, g: number, b: number) {
+        object.diffuseColor = new BABYLON.Color3(r, g, b);
+    }
+
+    setTexture(object: BABYLON.Material, url: string, scene: BABYLON.Scene) {
+        object.diffuseTexture = new BABYLON.Texture(url, scene);
+    }
+
+    animate() {
+        this.scene.render();
+        this.find(this.meshes, 'id', 'kamion').map((item: BABYLON.Mesh) => {
+            item.position.x += this.speed / 100;
+        });
+        this.find(this.meshes, 'id', 'kolesa').map((item: BABYLON.Mesh) => {
+            item.rotation.z += this.speed / 100;
+        });
+    }
+
+    changeMatCol() {
+        this.find(this.materials, 'id', 'naklad').map((item => {
+            this.changeColor(item, Math.random(), Math.random(), Math.random());
+        }));
     }
 }
+
